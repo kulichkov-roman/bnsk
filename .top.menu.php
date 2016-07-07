eccef958f680d645a0875c19a2ad5062edb37815
Roman Kulichkov <roman@kulichkov.pro>
2016-07-07 14:54:24 +0600

    Добавил главное меню в git

---
+++ .top.menu.php
@@ -0,0 +1,64 @@
+<?
+$aMenuLinks = Array(
+    Array(
+        "Компания",
+        "company/",
+        Array(),
+        Array(
+            "icon" => "building",
+            "subtext" => "О нашей компании"
+        ),
+        ""
+    ),
+    Array(
+        "Информация",
+        "info/",
+        Array(),
+        Array(
+            "icon" => "line2-info",
+            "subtext" => "Новости и статьи"
+        ),
+        ""
+    ),
+    Array(
+        "Курсы",
+        "study/",
+        Array(),
+        Array(
+            "icon" => "study",
+            "subtext" => "Обучение"
+        ),
+        ""
+    ),
+    Array(
+        "Услуги",
+        "services/",
+        Array(),
+        Array(
+            "icon" => "line2-briefcase",
+            "subtext" => "Чем полезны"
+        ),
+        ""
+    ),
+    Array(
+        "Каталог",
+        "catalog/",
+        Array(),
+        Array(
+            "icon" => "line2-tag",
+            "subtext" => "Наши товары"
+        ),
+        ""
+    ),
+    Array(
+        "Портфолио",
+        "portfolio/",
+        Array(),
+        Array(
+            "icon" => "line2-check",
+            "subtext" => "Успешные проекты"
+        ),
+        ""
+    ),
+);
+?>
