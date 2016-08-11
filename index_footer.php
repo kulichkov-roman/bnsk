1d905e4e96e2f408b737493d6490dbfce167f3cf
Roman Kulichkov <roman@kulichkov.pro>
2016-08-11 18:57:09 +0700

    Добавил шаблон index_footer.php, картинку с мужиковм с пивом

---
+++ index_footer.php
@@ -0,0 +1,26 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<div class="line topmargin-sm">
+</div>
+<div class="section nobg notopmargin nopadding footer-stick">
+	<div class="container clearfix">
+		<div class="row">
+			<div class="col-md-7">
+ <img alt="Доверьтесь нам" src="/include/images/bottom-trust.jpg">
+			</div>
+			<div class="col-md-5 topmargin-sm">
+				<div class="heading-block nobottomborder">
+					<h2>Вы в надежных руках.</h2>
+ <span class="ls1">5 причин, почему мы лучшие.</span>
+				</div>
+				<ul class="iconlist iconlist-large iconlist-color">
+					<li><i class="icon-ok"></i> Лидер рынка с 1997 года</li>
+					<li><i class="icon-ok"></i> Лучшие сорта пива</li>
+					<li><i class="icon-ok"></i> Пивной бизнес&nbsp;«под ключи»</li>
+					<li><i class="icon-ok"></i> Профессиональная команда</li>
+					<li><i class="icon-ok"></i> Оперативная доставка 7 дней в неделю</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+ <br>
