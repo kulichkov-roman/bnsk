7e334650c3d69c6b3a554df223025c2bd49fe965
Kulichkov Roman <roman@kulichkov.pro>
2016-09-19 21:47:08 +0700

    +/ajax/

--- bitrix/templates/cosmos_construct_s1/header.php
+++ bitrix/templates/cosmos_construct_s1/header.php
@@ -13,7 +13,7 @@ CModule::IncludeModule("boxsol.cosmos");
     <head>
         <!-- Stylesheets
         ============================================= -->
-        <link rel="shortcut icon" href="<?=SITE_DIR?>favicon.png">
+        <link rel="shortcut icon" href="<?=SITE_DIR?>favicon.ico">
         <link
             href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic&subset=latin,cyrillic'
             rel='stylesheet' type='text/css'>
