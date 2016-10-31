f1c75911f3c71af1d33e8e89821d35c996d579e1
Roman Kulichkov <roman@kulichkov.pro>
2016-10-31 16:39:38 +0700

    Удалил блок похожих новостей

--- bitrix/templates/cosmos_construct_s1/components/bitrix/news/news/detail.php
+++ bitrix/templates/cosmos_construct_s1/components/bitrix/news/news/detail.php
@@ -64,63 +64,6 @@ $this->setFrameMode(TRUE);
 				),
 				$component
 		); ?>
-
-		<?
-		/*global $noCurElement;
-		$noCurElement = array('!ID' => $ElementID);
-		?>
-		<? $APPLICATION->IncludeComponent(
-				"bitrix:news.list",
-				"related_posts",
-				Array(
-						"IBLOCK_TYPE"                     => $arParams["IBLOCK_TYPE"],
-						"IBLOCK_ID"                       => $arParams["IBLOCK_ID"],
-						"NEWS_COUNT"                      => 2,
-						"SORT_BY1"                        => 'ACTIVE_FROM',
-						"SORT_ORDER1"                     => $arParams["SORT_ORDER1"],
-						"SORT_BY2"                        => $arParams["SORT_BY2"],
-						"SORT_ORDER2"                     => $arParams["SORT_ORDER2"],
-						"FIELD_CODE"                      => $arParams["LIST_FIELD_CODE"],
-						"PROPERTY_CODE"                   => $arParams["LIST_PROPERTY_CODE"],
-						"DETAIL_URL"                      => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
-						"SECTION_URL"                     => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
-						"IBLOCK_URL"                      => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"],
-						"DISPLAY_PANEL"                   => $arParams["DISPLAY_PANEL"],
-						"SET_TITLE"                       => 'N',
-						"SET_STATUS_404"                  => 'N',
-						"INCLUDE_IBLOCK_INTO_CHAIN"       => 'N',
-						"CACHE_TYPE"                      => $arParams["CACHE_TYPE"],
-						"CACHE_TIME"                      => $arParams["CACHE_TIME"],
-						"CACHE_FILTER"                    => 'Y',
-						"CACHE_GROUPS"                    => $arParams["CACHE_GROUPS"],
-						"DISPLAY_TOP_PAGER"               => $arParams["DISPLAY_TOP_PAGER"],
-						"DISPLAY_BOTTOM_PAGER"            => $arParams["DISPLAY_BOTTOM_PAGER"],
-						"PAGER_TITLE"                     => 'N',
-						"PAGER_TEMPLATE"                  => $arParams["PAGER_TEMPLATE"],
-						"PAGER_SHOW_ALWAYS"               => $arParams["PAGER_SHOW_ALWAYS"],
-						"PAGER_DESC_NUMBERING"            => $arParams["PAGER_DESC_NUMBERING"],
-						"PAGER_DESC_NUMBERING_CACHE_TIME" => $arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
-						"PAGER_SHOW_ALL"                  => $arParams["PAGER_SHOW_ALL"],
-						"DISPLAY_DATE"                    => $arParams["DISPLAY_DATE"],
-						"DISPLAY_NAME"                    => "Y",
-						"DISPLAY_PICTURE"                 => $arParams["DISPLAY_PICTURE"],
-						"DISPLAY_PREVIEW_TEXT"            => $arParams["DISPLAY_PREVIEW_TEXT"],
-						"PREVIEW_TRUNCATE_LEN"            => 150,
-						"ACTIVE_DATE_FORMAT"              => $arParams["LIST_ACTIVE_DATE_FORMAT"],
-						"USE_PERMISSIONS"                 => $arParams["USE_PERMISSIONS"],
-						"GROUP_PERMISSIONS"               => $arParams["GROUP_PERMISSIONS"],
-						"FILTER_NAME"                     => 'noCurElement',
-						"HIDE_LINK_WHEN_NO_DETAIL"        => $arParams["HIDE_LINK_WHEN_NO_DETAIL"],
-						"CHECK_DATES"                     => $arParams["CHECK_DATES"],
-						"NL_USE_IMAGE_RESIZE"             => $arParams["NL_USE_IMAGE_RESIZE"],
-						"NL_IMAGE_RESIZE_WIDTH"           => $arParams["NL_IMAGE_RESIZE_WIDTH"],
-						"NL_IMAGE_RESIZE_HEIGHT"          => $arParams["NL_IMAGE_RESIZE_HEIGHT"],
-						"NL_IMAGE_RESIZE_TYPE"            => $arParams["NL_IMAGE_RESIZE_TYPE"],
-				),
-				$component
-		);/*?>
-
 	</div>
-
 </div><!-- .postcontent end -->
 
