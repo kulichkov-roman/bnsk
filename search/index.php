3a6f1cc361055c5f53e9b6b2c963fe160e802c73
Roman Kulichkov <roman@kulichkov.pro>
2016-10-31 18:29:24 +0700

    +search index.php

---
+++ search/index.php
@@ -0,0 +1,53 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Поиск по сайту");
+?>
+<?
+$APPLICATION->IncludeComponent(
+	"bitrix:search.page", 
+	"clear", 
+	array(
+		"RESTART" => "N",
+		"NO_WORD_LOGIC" => "N",
+		"CHECK_DATES" => "N",
+		"USE_TITLE_RANK" => "N",
+		"DEFAULT_SORT" => "rank",
+		"FILTER_NAME" => "",
+		"arrFILTER" => array(
+		),
+		"SHOW_WHERE" => "Y",
+		"arrWHERE" => array(
+		),
+		"SHOW_WHEN" => "N",
+		"PAGE_RESULT_COUNT" => "50",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "3600",
+		"USE_LANGUAGE_GUESS" => "Y",
+		"USE_SUGGEST" => "N",
+		"SHOW_ITEM_TAGS" => "Y",
+		"TAGS_INHERIT" => "Y",
+		"SHOW_ITEM_DATE_CHANGE" => "Y",
+		"SHOW_ORDER_BY" => "Y",
+		"SHOW_TAGS_CLOUD" => "N",
+		"SHOW_RATING" => "",
+		"RATING_TYPE" => "",
+		"PATH_TO_USER_PROFILE" => "",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Результаты поиска",
+		"PAGER_SHOW_ALWAYS" => "Y",
+		"PAGER_TEMPLATE" => "",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"STRUCTURE_FILTER" => "structure",
+		"NAME_TEMPLATE" => "",
+		"SHOW_LOGIN" => "Y"
+	),
+	false
+);
+?>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");?>
